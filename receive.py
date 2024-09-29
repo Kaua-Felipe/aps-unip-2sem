baba5b37a2d2d7d5bd35f6048130c9ce8d4d0efd
Kauã Felipe Alves <kauafelipeoficial191@gmail.com>
2024-09-29 18:33:28 -0300

    Fazendo alterações na cifra de césar no momento da criptografia, e adicionando uma chave para poder criptografar junto à cifra.

--- receive.py
+++ receive.py
@@ -5,12 +5,13 @@ def main():
     connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
     channel = connection.channel()
 
-    channel.queue_declare(queue='hello')
+    channel.queue_declare(queue='fila')
 
     def callback(ch, method, properties, body):
         print(f" [x] Received {body}")
 
-    channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
+
+    channel.basic_consume(queue='fila', on_message_callback=callback, auto_ack=True)
 
     print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
--- send.py
+++ send.py
@@ -1,19 +1,32 @@
 import pika
 import random
 
-def criptografar_texto_to_cesar(msg):
+def criptografar_texto_to_cesar(msg, chave):
+    listNumber = "0123456789"
+    soma = 0
+
     msg_final = ""
     words = list(msg)
     newwords = [None] * len(msg)
+    ascChave = [None] * len(chave)
+
+    # Loop para transformar a chave em um número
+    for index, item in enumerate(chave):
+        if item in listNumber:
+            ascChave[index] = int(item)
+        else:
+            ascChave[index] = ord(item)
+        soma += ascChave[index]
+    media = int(soma / len(ascChave))
 
     # Primeiro loop
     for i in range(len(msg)):
         letter = msg[i]
         asc = ord(letter)
-        if asc == 32:
-            ready = 127 - random.randint(1, 10)
-        else:
-            ready = asc - random.randint(1, 10)
+        print(asc)
+        ready = asc - media
+        if ready < 32:
+            ready = (asc + 100) - media
         letter = chr(ready)
         words[i] = letter
 
@@ -63,15 +76,11 @@ def descriptografar_cesar_to_texto(msg):
 
 connection = pika .BlockingConnection(pika.ConnectionParameters("localhost"))
 channel = connection.channel()
-channel.queue_declare(queue="hello")
+channel.queue_declare(queue="fila")
 
-channel.basic_publish(exchange="", routing_key="hello", body=criptografar_texto_to_cesar("Meu nome é Kauã Felipe Alves, e a senha pro e-mail é kkk123Joao!"))
+frase = input("Frase a ser enviada: ")
+
+channel.basic_publish(exchange="", routing_key="fila", body=criptografar_texto_to_cesar(frase, "Agora, qualquer chave que eu colocar aqui, irá criptografar facilmente!"))
 print(" [x] Send Message!")
 
 connection.close()
-
-
-
-criptografado = criptografar_texto_to_cesar("kaua Felipe Alves")
-descriptografado = descriptografar_cesar_to_texto(criptografado)
-print(descriptografado)
